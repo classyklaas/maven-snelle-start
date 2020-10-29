eafcbc3707a4d09e2adb0dfa3a416fe7a1150c5e
classyklaas <klaasjankostelijk@gmail.com>
2020-10-29 19:21:04 +0100

    pom.xml en application.java aangepast

--- src/main/java/nl/klaasjankostelijk/Application.java
+++ src/main/java/nl/klaasjankostelijk/Application.java
@@ -1,7 +1,18 @@
 package nl.klaasjankostelijk;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     
+	public void greet(){
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hoi");
+		
+		for (String greeting : greetings){
+	System.out.println("Gegroet: " + greeting);}
+	}
+	
     public Application() {
         System.out.println ("Inside Application");
     }
@@ -10,5 +21,6 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	    Application app = new Application();
+		app.greet();
     }
 }
